961038888f526bec167ac019d9483b44c6adfda1
signature18632 <intmax18@gmail.com>
2025-05-30 07:01:58 +0700

    withdrawal cache options

--- packages/api/src/routes/utils.ts
+++ packages/api/src/routes/utils.ts
@@ -3,9 +3,14 @@ import { etag } from "hono/etag";
 import { CACHE_TIMEOUTS } from "../constants";
 import { cacheMiddleware } from "../middlewares";
 
+interface CacheOptions {
+  detailCacheTimeout?: number;
+}
+
 interface RouterConfig {
   listHandler: (c: Context) => Promise<any>;
   getHandler: (c: Context) => Promise<any>;
+  options?: CacheOptions;
 }
 
 export const createBaseRouter = (config: RouterConfig): Hono => {
@@ -14,7 +19,9 @@ export const createBaseRouter = (config: RouterConfig): Hono => {
   router.use("/", (c, next) => cacheMiddleware(c, next, CACHE_TIMEOUTS.LIST));
 
   router.use("/:hash", etag());
-  router.use("/:hash", (c, next) => cacheMiddleware(c, next, CACHE_TIMEOUTS.DETAIL));
+  router.use("/:hash", (c, next) =>
+    cacheMiddleware(c, next, config.options?.detailCacheTimeout || CACHE_TIMEOUTS.DETAIL),
+  );
 
   router.get("/", config.listHandler);
   router.get("/:hash", config.getHandler);
