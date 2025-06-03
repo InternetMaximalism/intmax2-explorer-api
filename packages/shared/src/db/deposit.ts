218fdd498ed0e6a8933a8c5357a32320aa604549
signature18632 <intmax18@gmail.com>
2025-06-03 11:28:22 +0700

    Feat/total l1 wallet count (#87)
    * added total l1 wallet count
    
    * deposit address count

--- packages/shared/src/db/deposit.ts
+++ packages/shared/src/db/deposit.ts
@@ -1,5 +1,6 @@
 import type { CollectionReference, Query } from "@google-cloud/firestore";
-import { FIRESTORE_COLLECTIONS } from "../constants";
+import { FIRESTORE_COLLECTIONS, FIRESTORE_MAX_BATCH_SIZE } from "../constants";
+import { logger } from "../lib";
 import type { DepositData, DepositFilters, DepositInput } from "../types";
 import { BaseRepository } from "./base";
 import { db } from "./firestore";
@@ -43,5 +44,40 @@ export class Deposit extends BaseRepository<DepositData, DepositFilters, Deposit
 
   async getDepositByDepositHash(hash: string) {
     return this.getByHash(hash);
+  }
+
+  async getExistingAddressCount(addresses: string[]) {
+    if (!addresses.length) return 0;
+
+    const normalizedAddresses = [...new Set(addresses.map((addr) => addr.toLowerCase()))];
+    const existingSet = new Set<string>();
+
+    const batchPromises = [];
+
+    for (let i = 0; i < normalizedAddresses.length; i += FIRESTORE_MAX_BATCH_SIZE) {
+      const batch = normalizedAddresses.slice(i, i + FIRESTORE_MAX_BATCH_SIZE);
+
+      const batchPromise = this.collection
+        .where("sender", "in", batch)
+        .select("sender")
+        .get()
+        .then((snapshot) => {
+          snapshot.docs.forEach((doc) => {
+            const data = doc.data();
+            console.log("data", data);
+            if (data?.sender) {
+              existingSet.add(data.sender.toLowerCase());
+            }
+          });
+        })
+        .catch((error) => {
+          logger.error(`Error fetching existing addresses in batch: ${error.message}`);
+        });
+
+      batchPromises.push(batchPromise);
+    }
+
+    await Promise.all(batchPromises);
+    return existingSet.size;
   }
 }
