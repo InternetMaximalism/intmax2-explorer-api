744e39722bc243a7ea3b37f77be7a326ced5ef12
signature18632 <intmax18@gmail.com>
2025-06-03 15:43:54 +0700

    update cache block time

--- packages/api/src/routes/blocks.route.ts
+++ packages/api/src/routes/blocks.route.ts
@@ -1,10 +1,14 @@
 import { etag } from "hono/etag";
 import * as blocksController from "../controllers/blocks.controller";
+import { CACHE_TIMEOUTS } from "./../constants";
 import { createBaseRouter } from "./utils";
 
 export const blocksRoute = createBaseRouter({
   listHandler: blocksController.listBlocks,
   getHandler: blocksController.getBlock,
+  options: {
+    detailCacheTimeout: CACHE_TIMEOUTS.MODIFIED_DETAIL,
+  },
 });
 
 blocksRoute.use("/:hash/validityProof", etag());
