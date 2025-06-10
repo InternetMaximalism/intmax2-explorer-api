419bf3946cc1aa14afc0dad8db62f1c1f7bc187e
signature18632 <intmax18@gmail.com>
2025-06-10 11:27:49 +0700

    update modules and node version

--- packages/shared/src/validations/hash.validation.test.ts
+++ packages/shared/src/validations/hash.validation.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { HashValidationType, hashValidation } from "./hash.validation";
+import { type HashValidationType, hashValidation } from "./hash.validation";
 
 describe("Hash Validation Object Schema", () => {
   it("should validate a valid hash object", () => {
