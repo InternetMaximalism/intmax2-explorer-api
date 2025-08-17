5991e261c2d3b01faa59410051f6bfb46b88381d
signature18632 <intmax18@gmail.com>
2025-08-17 07:05:34 +0700

    update modules (#177)
    * update modules
    
    * update ci
    
    * setup ci
    
    * cis

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     workspace: ["packages/*"],
+    coverage: {
+      reporter: ["text", "lcov", "html"],
+      reportsDirectory: "./coverage",
+    },
   },
 });
